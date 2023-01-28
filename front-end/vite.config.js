9b26899da92a00d66825870d55fbd56a6488238b
Max Kavaliou <mkovaliou@gmail.com>
2023-01-28 22:12:20 +0300

    Added splitVendorChunkPlugin to vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     host: "0.0.0.0",
     port: 4444,
   },
-})
+});
