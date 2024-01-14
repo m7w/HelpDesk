77b0613ea651ea10042b4a8d640daf1859e280e2
Max Kavaliou <mkovaliou@gmail.com>
2024-01-15 00:15:53 +0300

    Moved to new Spring Security

--- back-end/src/main/java/com/training/helpdesk/config/SecurityConfig.java
+++ back-end/src/main/java/com/training/helpdesk/config/SecurityConfig.java
@@ -2,33 +2,33 @@ package com.training.helpdesk.config;
 
 import java.util.Collections;
 
-import com.training.helpdesk.security.JwtRequestFilter;
-import com.training.helpdesk.security.util.JwtUtils;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.training.helpdesk.security.JwtRequestFilter;
+import com.training.helpdesk.security.util.JwtUtils;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final JwtUtils jwtUtils;
@@ -47,40 +47,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    protected AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)
+            throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors().configurationSource((CorsConfigurationSource) request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowCredentials(true);
+                    config.setAllowedOrigins(Collections.singletonList(allowedOrigin));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setExposedHeaders(Collections.singletonList("Authorization"));
+                    config.setMaxAge(3600L);
+                    return config;
+                })
+                .and()
+                .csrf().disable()
+                .addFilterBefore(jwtRequestFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(authorized -> authorized
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic();
 
-        http
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .cors().configurationSource((CorsConfigurationSource) request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowCredentials(true);
-                config.setAllowedOrigins(Collections.singletonList(allowedOrigin));
-                config.setAllowedMethods(Collections.singletonList("*"));
-                config.setAllowedHeaders(Collections.singletonList("*"));
-                config.setExposedHeaders(Collections.singletonList("Authorization"));
-                config.setMaxAge(3600L);
-                return config;
-            })
-            .and()
-            .csrf().disable()
-            .addFilterBefore(jwtRequestFilter(), BasicAuthenticationFilter.class)
-            .authorizeRequests()
-            .antMatchers("/api/auth/login").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
+        return http.build();
     }
 }
